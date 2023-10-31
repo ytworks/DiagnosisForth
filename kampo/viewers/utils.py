fa391cca426f243358a5b0bc0c8c7a46fa5d9cd2
ytworks <yuzo.takagi.private@gmail.com>
2023-10-31 14:58:05 +0900

    3D viewer

--- kampo/viewers/utils.py
+++ kampo/viewers/utils.py
@@ -1,11 +1,67 @@
+from ..interactions import analyzer
+import pandas as pd
+import py3Dmol
 
-color_map = {
-    "hydrophobic": [0.90, 0.10, 0.29],
-    "hbond": [0.26, 0.83, 0.96],
-    "waterbridge": [1.00, 0.88, 0.10],
-    "saltbridge": [0.67, 1.00, 0.76],
-    "pistacking": [0.75, 0.94, 0.27],
-    "pication": [0.27, 0.60, 0.56],
-    "halogen": [0.94, 0.20, 0.90],
-    "metal": [0.90, 0.75, 1.00],
-}
+
+def show_3d_interactions(pdb_file, ligand_name='UNL'):
+    """Show 3D interactions of a complex.
+
+    Parameters
+    ----------
+    pdb_file : str
+        Path to a PDB file of a complex.
+    """
+    bond_types = ["hydrophobic",
+                  "hbond",
+                  "waterbridge",
+                  "saltbridge",
+                  "pistacking",
+                  "pication",
+                  "halogen"]
+    colors = {"hydrophobic": "red",
+              "hbond": "blue",
+              "waterbridge": "blueCarbon",
+              "saltbridge": "white",
+              "pistacking": "yellow",
+              "pication": "orange",
+              "halogen": "magenta"}
+
+    interactions_by_site = analyzer.retrieve_plip_interactions(pdb_file)
+    index_of_selected_site = 0
+    selected_site = list(interactions_by_site.keys())[index_of_selected_site]
+    dfs = []
+    complete_dfs = {}
+    for bond_type in bond_types:
+        df = analyzer.create_df_from_binding_site(
+            interactions_by_site[selected_site],
+            interaction_type=bond_type)
+        complete_dfs[bond_type] = df
+        df = df[["RESNR", "RESTYPE", "LIGCOO", "PROTCOO"]]
+        df['BONDTYPE'] = bond_type
+        dfs.append(df)
+    df = pd.concat(dfs, axis=0)
+
+    view = py3Dmol.view()
+    view.addModel(open(pdb_file, 'r').read(), 'pdb')
+    view.setStyle({"cartoon": {"color": "grey"}})
+    LIG = [ligand_name]
+    view.addStyle({'and': [{'resn': LIG}]},
+                  {'stick': {'colorscheme': 'greenCarbon', 'radius': 0.3}})
+    view.setViewStyle({'style': 'outline', 'color': 'black', 'width': 0.1})
+    for idx, rows in df.iterrows():
+        view.setStyle({'resi': rows["RESNR"]},
+                      {'stick': {'colorscheme': 'whiteCarbon', 'radius': 0.2}})
+        sx, sy, sz = rows["LIGCOO"]
+        ex, ey, ez = rows["PROTCOO"]
+        view.addCylinder({"start": dict(x=sx, y=sy, z=sz),
+                          "end":   dict(x=ex, y=ey, z=ez),
+                          "color": colors[rows["BONDTYPE"]],
+                          "radius": .15,
+                          "dashed": True,
+                          "fromCap": 1,
+                          "toCap": 1,
+                          })
+    view.setViewStyle({'style': 'outline', 'color': 'black', 'width': 0.1})
+
+    view.zoomTo()
+    return view, complete_dfs
