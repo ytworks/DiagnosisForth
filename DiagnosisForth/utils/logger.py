f220a2a47c97452a7a6ed4c4214e9feb160ffc9f
ytworks <yuzo.takagi.private@gmail.com>
2025-10-17 04:12:42 +0900

    mf

--- DiagnosisForth/utils/logger.py
+++ DiagnosisForth/utils/logger.py
@@ -6,7 +6,7 @@ import requests
 
 def _send_log(text: str) -> None:
     try:
-        webhook_url = "https://hooks.slack.com/services/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"
+        webhook_url = "https://hooks.slack.com/services/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"
         payload = {"text": text}
         requests.post(webhook_url, json=payload, timeout=5)
     except:
